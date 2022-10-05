426dd3796856831d22a603d5d27b43f996a62fba
kitagawarisa <kitakawauso@gmail.com>
2022-10-05 20:55:39 +0900

    :+1: global navigation added

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// fadeout
+$(window).on("load", function () {
+  $("body").removeClass("fadeout");
+});
+
+$(function () {
+  $('a:not([href^="#"]):not([target])').on("click", function (e) {
+    e.preventDefault();
+    url = $(this).attr("href");
+    if (url !== "") {
+      $("body").addClass("fadeout");
+      setTimeout(function () {
+        window.location = url;
+      }, 800);
+    }
+    return false;
+  });
+});
+
+// global navigation
+$(".open-btn").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
+  $("#global-nav").toggleClass("panel-active"); //ナビゲーションにpanel-activeクラスを付与
+});
+
+$("#global-nav a").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".open-btn").removeClass("active"); //ボタンの activeクラスを除去し
+  $("#global-nav").removeClass("panel-active"); //ナビゲーションのpanel-activeクラスも除去
+});
+
+// slider
 $(function () {
   $(".slider-ui").slick({
     arrows: true,
